91e6404b263d82b7cbb4355060ae44fa1de23385
Ayush shivde <shivdeayush09@gmail.com>
2025-05-19 01:38:17 +0530

    Added homepage and deploy scripts

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/React-TODO_project/',
   plugins: [react() , tailwindcss()],
 })
